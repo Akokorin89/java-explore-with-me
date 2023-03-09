36bb7d0634f43239d35df8e303dc149c3a7129b9
Alexandr Kokorin <Akokorin89@yandex.ru>
2023-03-09 19:31:20 +0300

    fix docker #1

--- ewm-stats-service/src/main/java/com/example/ewmstatsservice/repository/StatsRepository.java
+++ ewm-stats-service/src/main/java/com/example/ewmstatsservice/repository/StatsRepository.java
@@ -5,7 +5,6 @@ import com.example.ewmstatsservice.model.EndpointHit;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
-
 import java.time.LocalDateTime;
 import java.util.List;
 
