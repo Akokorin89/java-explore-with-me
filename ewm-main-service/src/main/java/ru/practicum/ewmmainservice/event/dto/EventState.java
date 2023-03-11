e97dd29e947715f0f4e42122c69f5e73cda57a0e
Alexandr Kokorin <Akokorin89@yandex.ru>
2023-03-11 09:02:44 +0300

    delete main service

--- ewm-main-service/src/main/java/ru/practicum/ewmmainservice/event/dto/EventState.java
+++
@@ -1,7 +0,0 @@
-package ru.practicum.ewmmainservice.event.dto;
-
-public enum EventState {
-    PENDING,
-    PUBLISHED,
-    CANCELED
-}
--- ewm-main-service/src/main/java/ru/practicum/ewmmainservice/exception/EventSortException.java
+++
@@ -1,7 +0,0 @@
-package ru.practicum.ewmmainservice.exception;
-
-public class EventSortException extends RuntimeException {
-    public EventSortException(String message) {
-        super(message);
-    }
-}
